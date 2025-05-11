bd1319748b1526792b587982ac798587ad8ee343
22261A0537 <pnikhil_cse220537@mgit.ac.in>
2025-05-11 14:53:28 +0530

    exploring get, post patch

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 
 const express = require('express');
 const app = express();
-app.use('/nikhil',(req, res) => {
-    res.end('hello namste dev!');
-})
-app.use('/test', (req, res) => {
-    res.end('wat');
-})
+// app.use('/', (req, res) => {
+//     res.end('hello hello');
+// })
+app.get('/user', (req, res) => {
+    res.send({firstname:'nikhil',lastname:'patil'});
+});
+// app.use('/', (req, res) => {
+//     res.end('hello hello');
+// })
+app.post('/user', (req, res) => {
+    res.send('data successfully saved to DB');
+});
+app.patch('/user', (req, res) => {
+    res.send('successfully updated');
+});
 app.listen(7777, () => {
     console.log('server is listening the incoming requests from the port 7777');
-})
+});
 
